e95b9f37a7ba533094ce3dfd787aa2a5354787fb
ThushaniSankalpana <witharanathushani.sankalpana@gmail.com>
2023-09-23 21:32:33 +0530

    Initial Commit

---
+++ iOS_FinalProject/iOS_FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_FinalProjectApp.swift
+//  iOS_FinalProject
+//
+//  Created by Lakna Attigala on 2023-09-23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
